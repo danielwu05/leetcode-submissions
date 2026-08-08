da69209c217b660dd1608482245ecbc82c7b5367
Daniel Wu <danielwu288@gmail.com>
2026-08-08 18:18:08 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.6 MB (67.78%)

---
+++ 0392-is-subsequence/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int isSubsequence(string s, string t) {
+        int ptr1 = 0, ptr2 = 0;
+        while (ptr2 < t.size()) {
+            if (s[ptr1] == t[ptr2]) {
+                ptr1++;
+                ptr2++;
+            }
+            else {
+                ptr2++;
+            }
+        }
+        if (ptr1 == s.size()) return true;
+        return false;
+    }
+};
